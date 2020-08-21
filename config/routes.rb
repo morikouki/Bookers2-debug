3485a5bbf82017c07c3af5585c4d70aa256f4595
vagrant <akzouksia@icloud.com>
2020-08-21 07:40:19 +0000

    [Finish]4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+  	resource :relationships, only: [:create, :destroy]
+  	get :follows, on: :member # 追加
+    get :followers, on: :member # 追加
+  end
   resources :books do
   	resource :favorites, only: [:create, :destroy]
   	resources :comments, only: [:create, :destroy]
