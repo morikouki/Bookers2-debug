dd8318518a9f4d9ee243f61624ec2050721130a3
vagrant <akzouksia@icloud.com>
2020-08-22 07:59:00 +0000

    [Add]map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(function() {
+  return $('#user_postcode').jpostal({
+    postcode: ['#user_postcode'],
+    address: {
+      '#user_prefecture_code': '%3',
+      '#user_address_city': '%4',
+      '#user_address_street': '%5%6%7',
+    },
+  });
+});
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  def address
+    [address_street, address_city, postcode, prefecture_code].compact.join(', ')
+  end
+  geocoded_by :address, latitude: :lat, longitude: :lon
 end
